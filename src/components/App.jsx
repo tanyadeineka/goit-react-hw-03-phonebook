507b74d6bd6da5aa7c4fd592d9a070768f6516e2
Tetiana Deineka <tata4ka666@i.ua>
2022-12-20 21:16:12 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import React, { Component } from "react";
 import { nanoid } from 'nanoid';
 import { ContactForm } from "./ContactForm/ContactForm";
 import { Filter } from "./Filter/Filter";
@@ -15,6 +15,22 @@ export class App extends Component {
     filter: '',
     name: '',
     number: '',
+  };
+
+  componentDidMount() {
+    const contactsFromLocalStorage = localStorage.getItem("contactList");
+    const parsedContacts = JSON.parse(contactsFromLocalStorage);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    const prevStateContacts = prevState.contacts;
+    const nextStayContacts = this.state.contacts;
+    if (prevStateContacts !== nextStayContacts) {
+      localStorage.setItem("contactList", JSON.stringify(nextStayContacts));
+    }
   };
 
   handleChange = event => {
